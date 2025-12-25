a2ffa3d342ff07cfad4d6b3a5cbdf89909cb514c
elpepe1515 <elpepedasfas@gmail.com>
2025-12-24 21:16:58 -0500

    xd

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,12 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta name="description" content="Cementerio de Secretos v2.3 - UI/UX Móvil Optimizada y Navidad Mejorada" />
         <meta name="theme-color" content="#000000" />
+        <meta property="og:title" content="Cementerio de Secretos v2.3" />
+        <meta property="og:description" content="Comparte tus secretos anónimamente - 100% privado, respuestas, reacciones y más" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://cementerio-de-secretos-y-confesiones.vercel.app/" />
+        <meta property="twitter:title" content="Cementerio de Secretos v2.3" />
+        <meta property="twitter:description" content="Comparte tus secretos anónimamente - 100% privado, respuestas, reacciones y más" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
